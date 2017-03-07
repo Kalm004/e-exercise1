cfd2581995363b4ef7c201e4db62c74b214ef654
Abraham Romero <abraham.romero@edataconsulting.de>
2017-03-07 16:09:14 +0000

    Use waterline instead of orm

--- initializers/database.js
+++ initializers/database.js
@@ -1,15 +1,56 @@
 'use strict';
 
-let orm = require("orm");
+let waterline = require("waterline");
+let orm = new waterline();
+
+var postgreAdapter = require('sails-postgresql'),
 
 module.exports = () => {
+    var config = {
+
+        // Setup Adapters
+        // Creates named adapters that have been required
+        adapters: {
+            'default': postgreAdapter,
+            postgresql: postgreAdapter
+        },
+
+        // Build Connections Config
+        // Setup connections using the named adapter configs
+        connections: {
+            myLocalPostgreSql: {
+                adapter: 'postgresql',
+                host: 'localhost',
+                database: 'foobar'
+            }
+        },
+
+        defaults: {
+            migrate: 'alter'
+        }
+
+    };
+
+    var User = Waterline.Model.extend({
+
+        identity: 'user',
+        connection: 'myLocalDisk',
+
+        attributes: {
+            first_name: 'string',
+            last_name: 'string'
+        }
+    });
+
+    orm.registerModel(User);
+
     let opts = {
         database : "postgres",
         protocol : "postgres",
         host     : "127.0.0.1",
         port     : 5432,         // optional, defaults to database default
         user     : "postgres",
-        password : "admin",
+        password : "postgres",
     };
     return new Promise(function (resolve, reject) {
         orm.connect(opts, function (err, db) {
