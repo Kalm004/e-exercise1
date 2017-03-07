3dc1b60af13ca3f57773fd73264615d61c9b3780
Kalm004 <kalm004@gmail.com>
2017-03-07 18:47:28 +0000

    Reverse use of waterline

--- initializers/database.js
+++ initializers/database.js
@@ -1,56 +1,15 @@
 'use strict';
 
-let waterline = require("waterline");
-let orm = new waterline();
-
-var postgreAdapter = require('sails-postgresql'),
+let orm = require("orm");
 
 module.exports = () => {
-    var config = {
-
-        // Setup Adapters
-        // Creates named adapters that have been required
-        adapters: {
-            'default': postgreAdapter,
-            postgresql: postgreAdapter
-        },
-
-        // Build Connections Config
-        // Setup connections using the named adapter configs
-        connections: {
-            myLocalPostgreSql: {
-                adapter: 'postgresql',
-                host: 'localhost',
-                database: 'foobar'
-            }
-        },
-
-        defaults: {
-            migrate: 'alter'
-        }
-
-    };
-
-    var User = Waterline.Model.extend({
-
-        identity: 'user',
-        connection: 'myLocalDisk',
-
-        attributes: {
-            first_name: 'string',
-            last_name: 'string'
-        }
-    });
-
-    orm.registerModel(User);
-
     let opts = {
         database : "postgres",
         protocol : "postgres",
         host     : "127.0.0.1",
         port     : 5432,         // optional, defaults to database default
         user     : "postgres",
-        password : "postgres",
+        password : "admin",
     };
     return new Promise(function (resolve, reject) {
         orm.connect(opts, function (err, db) {
